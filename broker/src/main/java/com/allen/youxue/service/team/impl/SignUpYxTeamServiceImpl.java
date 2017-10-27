378075a696e4203294969452baa9af2c68b2fe3b
Allen <2319772333@qq.com>
2017-10-27 17:04:53 +0800

    *

--- broker/src/main/java/com/allen/youxue/service/team/impl/SignUpYxTeamServiceImpl.java
+++ broker/src/main/java/com/allen/youxue/service/team/impl/SignUpYxTeamServiceImpl.java
@@ -1,6 +1,8 @@
 package com.allen.youxue.service.team.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.allen.base.exception.BusinessException;
+import com.allen.service.broker.FindBrokerByZZService;
 import com.allen.youxue.dao.team.YxTeamDao;
 import com.allen.youxue.entity.team.Team;
 import com.allen.youxue.service.team.SignUpYxTeamService;
@@ -16,10 +18,16 @@ public class SignUpYxTeamServiceImpl implements SignUpYxTeamService {
 
     @Autowired
     private YxTeamDao yxTeamDao;
+    @Autowired
+    private FindBrokerByZZService findBrokerByZZService;
 
     @Override
     @Transactional
     public void signUp(long projectId, String zz, String yyDate, long teamHeadId) throws Exception {
+        JSONObject json = findBrokerByZZService.findAttop(zz);
+        if("0".equals(json.get("sflag").toString())){
+            throw new BusinessException("您目前不是学生，请先成为学生再申请");
+        }
         Team team = yxTeamDao.findByZzAndParentId(zz, teamHeadId);
         if(null != team){
             throw new BusinessException("你已经是该团的团员，不能重复报名");
